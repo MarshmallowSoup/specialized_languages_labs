0288a2d74fb38707c6a3e101a0f9af89ed14be1a
Volodymyr Lysetskyi <vlysetskyi@dedicatted.com>
2023-12-14 23:05:23 +0200

    finalized lab8

--- lab8/DataProcessing.py
+++ lab8/DataProcessing.py
@@ -2,65 +2,52 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Drawer:
-    def init(self, file_path):
+    def __init__(self, file_path):
         self.file_path = file_path
         self.data = None
 
     def read_data(self):
         self.data = pd.read_csv(self.file_path)
 
-    def plot_pie_chart(self, country_column):
+    def plot_time_series(self, region_column):
         if self.data is not None:
-            country_count = self.data[country_column].value_counts()
+            region_data = self.data.groupby(region_column).sum().transpose()
+            region_data.plot(figsize=(14, 8), title='Time Series Analysis by Region')
+            plt.xlabel('Date')
+            plt.ylabel('Total Value')  # You may adjust this based on your data
+            plt.legend(title=region_column, bbox_to_anchor=(1.05, 1), loc='upper left')
+            plt.tight_layout()
+            plt.show()
+            return plt
+
+    def plot_pie_chart(self, region_column):
+        if self.data is not None:
+            region_count = self.data[region_column].value_counts()
             plt.figure(figsize=(12, 12))
-            country_count.plot(kind='pie', autopct='%1.1f%%')
-            plt.title('Кругова діаграма кількості користувачів з кожного країни')
+            region_count.plot(kind='pie', autopct='%1.1f%%')
+            plt.title('Pie Chart: Distribution of Regions')
             plt.ylabel('')
             plt.axis('equal')
             plt.tight_layout()
             plt.show()
 
-            return plt
-
-    def plot_bar_chart(self, country_column, age_column):
+    def plot_bar_chart(self, region_column, value_column):
         if self.data is not None:
             plt.figure(figsize=(10, 6))
-            avg_age_by_country = self.data.groupby(country_column)[age_column].size()
-            avg_age_by_country.plot(kind='bar', color='skyblue')
-            plt.title('Середній вік користувачів з кожної країни')
-            plt.xlabel('Країна')
-            plt.ylabel('Середній вік')
+            avg_value_by_region = self.data.groupby(region_column)[value_column].sum()
+            avg_value_by_region.plot(kind='bar', color='skyblue')
+            plt.title('Bar Chart: Total Value by Region')
+            plt.xlabel('Region')
+            plt.ylabel('Total Value')
             plt.xticks(rotation=45)
             plt.grid(axis='y')
             plt.tight_layout()
             plt.show()
 
-            # Додано піддіаграму
-            plt.subplot(1, 3, 3)
-            self.plot_subscription_date_distribution()
-
-    def plot_subscription_date_distribution(self):
-        if self.data is not None:
-            subscription_date_count = self.data['Subscription Date'].value_counts()
-            subscription_date_count.plot(kind='line', marker='o', color='red')
-            plt.title('Розподіл кількості користувачів за датою підписки')
-            plt.xlabel('Дата підписки')
-            plt.ylabel('Кількість користувачів')
-            plt.grid(True)
-
-
-    def plot_unique_countries_count(self, country_column):
-        if self.data is not None:
-            plt.subplot(1, 3, 3)
-            unique_countries_count = self.data[country_column].nunique()
-            plt.bar('Кількість унікальних країн', unique_countries_count, color='green')
-            plt.title('Кількість унікальних компаній у даних')
-            plt.xlabel('Показник')
-            plt.ylabel('Кількість країн')
-            plt.grid(axis='y')
-            plt.tight_layout()
-
-            plt.show()
 
     def save(self, plot: plt):
-        pass
+        plot.savefig("./DIAG.png")
+        plot.savefig("./DIAG.svg")
+        plot.savefig("./DIAG.pdf")
+
+
